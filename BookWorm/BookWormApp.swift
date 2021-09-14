59c6444e081a6585ee8852f42453139421b926b4
David <david.r.rozenberg@gmail.com>
2021-09-15 01:00:07 +0200

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by DavidR on 15/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
