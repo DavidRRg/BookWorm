ebfa26232b13eda3b0e23b7251935dbd959d5cd2
David <david.r.rozenberg@gmail.com>
2021-09-20 01:05:44 +0200

    BookWorm
    added book worm app up to step 7

---
+++ BookWorm/EmojiRatingView.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiRatingView.swift
+//  BookWorm
+//
+//  Created by DavidR on 15/09/2021.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView()
+    }
+}
---
+++ BookWorm/Persistence.swift
@@ -0,0 +1,8 @@
+//
+//  Persistence.swift
+//  BookWorm
+//
+//  Created by DavidR on 15/09/2021.
+//
+
+import Foundation
